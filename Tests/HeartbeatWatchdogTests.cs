63b7eab049064c878a65492b49b367ce8da51036
Saito Nakamura <saitonakamuramb@gmail.com>
2018-11-26 16:44:26 +0300

    :sparkles: :white_check_mark: Added external now beat support and added tests

---
+++ Tests/HeartbeatWatchdogTests.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Threading;
+using Moq;
+using saitonakamura.Watchdog;
+using Xunit;
+
+namespace Tests
+{
+    public class StartTests
+    {
+        [Fact]
+        public void Given1SecondAndStart_FiresEventBeforeSecondAndABit()
+        {
+            var handlerMock = new Mock<ITestHandler>();
+
+            var watchdog = new HeartbeatWatchdog(TestTimespans.Second);
+            watchdog.HeartbeatStopped += handlerMock.Object.Handle;
+
+            watchdog.Start();
+            Thread.Sleep(TestTimespans.SecondAndABit);
+
+            handlerMock.Verify(x => x.Handle(It.IsAny<object>(), It.IsAny<EventArgs>()), Times.AtLeastOnce);
+        }
+
+        [Fact]
+        public void Given1SecondAndNoStart_DoesntFireEvent()
+        {
+            var handlerMock = new Mock<ITestHandler>();
+
+            var watchdog = new HeartbeatWatchdog(TestTimespans.Second);
+            watchdog.HeartbeatStopped += handlerMock.Object.Handle;
+
+            Thread.Sleep(TestTimespans.SecondAndABit);
+
+            handlerMock.Verify(x => x.Handle(It.IsAny<object>(), It.IsAny<EventArgs>()), Times.Never);
+        }
+    }
+
+    public class StartNewTests
+    {
+        [Fact]
+        public void Given1SecondAndStartNew_FiresEventBeforeSecondAndABit()
+        {
+            var handlerMock = new Mock<ITestHandler>();
+
+            HeartbeatWatchdog.StartNew(TestTimespans.Second, handlerMock.Object.Handle);
+
+            Thread.Sleep(TestTimespans.SecondAndABit);
+
+            handlerMock.Verify(x => x.Handle(It.IsAny<object>(), It.IsAny<EventArgs>()), Times.AtLeastOnce);
+        }
+    }
+
+    public class CancelTests
+    {
+        [Fact]
+        public void Given1SecondAndStartAndImmediateCancel_DoesntFireEvent()
+        {
+            var handlerMock = new Mock<ITestHandler>();
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            var watchdog = new HeartbeatWatchdog(TestTimespans.Second);
+            watchdog.HeartbeatStopped += handlerMock.Object.Handle;
+            watchdog.Start(cancellationTokenSource.Token);
+
+            cancellationTokenSource.Cancel();
+
+            Thread.Sleep(TestTimespans.SecondAndABit);
+
+            handlerMock.Verify(x => x.Handle(It.IsAny<object>(), It.IsAny<EventArgs>()), Times.Never);
+        }
+    }
+
+    public class CoupleBeatsTests
+    {
+        [Fact]
+        public void GivenTwoHalfSecondBeats_FiresEvent()
+        {
+            var handlerMock = new Mock<ITestHandler>();
+
+            var watchdog = HeartbeatWatchdog.StartNew(TestTimespans.Second, handlerMock.Object.Handle);
+
+            Thread.Sleep(TestTimespans.HalfSecond);
+            handlerMock.Verify(x => x.Handle(It.IsAny<object>(), It.IsAny<EventArgs>()), Times.Never);
+            watchdog.Beat();
+
+            Thread.Sleep(TestTimespans.HalfSecond);
+            handlerMock.Verify(x => x.Handle(It.IsAny<object>(), It.IsAny<EventArgs>()), Times.Never);
+            watchdog.Beat();
+
+            Thread.Sleep(TestTimespans.TwoSecondAndABit);
+            handlerMock.Verify(x => x.Handle(It.IsAny<object>(), It.IsAny<EventArgs>()), Times.AtLeastOnce);
+        }
+    }
+
+    public class BeatWithExternalNowTests
+    {
+        [Fact]
+        public void GivenExternalNow_FiresEvent()
+        {
+            var handlerMock = new Mock<ITestHandler>();
+
+            var watchdog =  new HeartbeatWatchdog(TestTimespans.Second);
+            watchdog.HeartbeatStopped += handlerMock.Object.Handle;
+            watchdog.Start();
+
+            Thread.Sleep(TestTimespans.HalfSecond);
+            watchdog.Beat(DateTime.Now);
+
+            Thread.Sleep(TestTimespans.TwoSecondAndABit);
+
+            handlerMock.Verify(x => x.Handle(It.IsAny<object>(), It.IsAny<EventArgs>()), Times.AtLeastOnce);
+        }
+
+        [Fact]
+        public void GivenExternalNowInTheFuture_DoesntFireEvent()
+        {
+            var handlerMock = new Mock<ITestHandler>();
+
+            var watchdog = new HeartbeatWatchdog(TestTimespans.Second);
+            watchdog.HeartbeatStopped += handlerMock.Object.Handle;
+            watchdog.Start();
+
+            Thread.Sleep(TestTimespans.HalfSecond);
+            watchdog.Beat(DateTime.Now.AddSeconds(3));
+
+            Thread.Sleep(TestTimespans.TwoSecondAndABit);
+
+            handlerMock.Verify(x => x.Handle(It.IsAny<object>(), It.IsAny<EventArgs>()), Times.Never);
+        }
+
+        [Fact]
+        public void GivenExternalNowInThePast_FiresEvent()
+        {
+            var handlerMock = new Mock<ITestHandler>();
+
+            var watchdog = new HeartbeatWatchdog(TestTimespans.Second);
+            watchdog.HeartbeatStopped += handlerMock.Object.Handle;
+            watchdog.Start();
+
+            Thread.Sleep(TestTimespans.HalfSecond);
+            watchdog.Beat(DateTime.Now.AddSeconds(-3));
+
+            Thread.Sleep(TestTimespans.SecondAndABit);
+
+            handlerMock.Verify(x => x.Handle(It.IsAny<object>(), It.IsAny<EventArgs>()), Times.AtLeastOnce);
+        }
+    }
+
+    public class IntIntervalsTests
+    {
+        // ReSharper disable once ConvertToConstant.Local
+        private readonly int _second = 1000;
+        // ReSharper disable once ConvertToConstant.Local
+        private readonly int _secondAndABit = 1200;
+
+        [Fact]
+        public void Given1SecondAndStartNew_FiresEventBeforeSecondAndABit()
+        {
+            var handlerMock = new Mock<ITestHandler>();
+
+            HeartbeatWatchdog.StartNew(_second, handlerMock.Object.Handle);
+
+            Thread.Sleep(_secondAndABit);
+
+            handlerMock.Verify(x => x.Handle(It.IsAny<object>(), It.IsAny<EventArgs>()), Times.AtLeastOnce);
+        }
+    }
+
+    public class LoggerTests
+    {
+        [Fact]
+        public void GivenInfoLogger_LogsStart()
+        {
+            var infoLoggerMock = new Mock<ILogger>();
+
+            HeartbeatWatchdog.StartNew(alertSpan: TestTimespans.Second,
+                stopHandler:(_, __) => { },
+                infoLogger: infoLoggerMock.Object.Info);
+
+            infoLoggerMock.Verify(x => x.Info("HeartbeatWatchdog  has been started"), Times.Once);
+        }
+    }
+
+    public static class TestTimespans
+    {
+        public static TimeSpan HalfSecond = TimeSpan.FromSeconds(0.5);
+        public static TimeSpan Second = TimeSpan.FromSeconds(1);
+        public static TimeSpan SecondAndABit = TimeSpan.FromSeconds(1).Add(TimeSpan.FromMilliseconds(200));
+        public static TimeSpan TwoSecondAndABit = TimeSpan.FromSeconds(2).Add(TimeSpan.FromMilliseconds(200));
+    }
+
+    public interface ITestHandler
+    {
+        void Handle(object sender, EventArgs eventArgs);
+    }
+
+    public interface ILogger
+    {
+        void Info(string obj);
+    }
+}
--- saitonakamura.Watchdog/HeartbeatWatchdog.cs
+++ saitonakamura.Watchdog/HeartbeatWatchdog.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace saitonakamura.Watchdog
 {
-    public class HeartbeatWatchdog : IDisposable
+    public class HeartbeatWatchdog
     {
         public event EventHandler<EventArgs> HeartbeatStopped = delegate { };
 
@@ -14,20 +14,21 @@ namespace saitonakamura.Watchdog
         private readonly Action<string> _debugLogger = delegate { };
         private readonly Action<Exception, string> _errorLogger = delegate { };
 
-        private CancellationTokenSource _cancelSource;
-        private CancellationTokenSource _innerCancellationTokenSource;
-
         // Thread shared var, be careful
         private long _lastBeatTotalMs;
 
         public HeartbeatWatchdog(int milliseconds, 
             string name = "",
+            EventHandler<EventArgs> stopHandler = null,
             Action<string> infoLogger = null, 
             Action<string> debugLogger = null,
             Action<Exception, string> errorLogger = null)
         {
             _alertMs = milliseconds;
             _name = name;
+
+            if (stopHandler != null)
+                HeartbeatStopped += stopHandler;
 
             if (infoLogger != null)
                 _infoLogger = infoLogger;
@@ -39,67 +40,53 @@ namespace saitonakamura.Watchdog
 
         public HeartbeatWatchdog(TimeSpan alertSpan,
             string name = null,
+            EventHandler<EventArgs> stopHandler = null,
             Action<string> infoLogger = null,
             Action<string> debugLogger = null,
             Action<Exception, string> errorLogger = null)
 
-            : this((int)alertSpan.TotalMilliseconds, name, infoLogger, debugLogger, errorLogger)
+            : this((int)alertSpan.TotalMilliseconds, name, stopHandler, infoLogger, debugLogger, errorLogger)
         {
         }
 
         public static HeartbeatWatchdog StartNew(TimeSpan alertSpan,
+            EventHandler<EventArgs> stopHandler,
             string name = null,
+            CancellationToken outerCancellationToken = default(CancellationToken),
             Action<string> infoLogger = null,
             Action<string> debugLogger = null,
             Action<Exception, string> errorLogger = null)
         {
-            return new HeartbeatWatchdog(alertSpan, name, infoLogger, debugLogger, errorLogger);
+            var heartbeatWatchdog = new HeartbeatWatchdog(alertSpan, name, stopHandler, infoLogger, debugLogger, errorLogger);
+            heartbeatWatchdog.Start(outerCancellationToken);
+            return heartbeatWatchdog;
         }
 
         public static HeartbeatWatchdog StartNew(int alertSpan,
+            EventHandler<EventArgs> stopHandler,
             string name = null,
+            CancellationToken outerCancellationToken = default(CancellationToken),
             Action<string> infoLogger = null,
             Action<string> debugLogger = null,
             Action<Exception, string> errorLogger = null)
         {
-            return new HeartbeatWatchdog(alertSpan, name, infoLogger, debugLogger, errorLogger);
+            var heartbeatWatchdog = new HeartbeatWatchdog(alertSpan, name, stopHandler, infoLogger, debugLogger, errorLogger);
+            heartbeatWatchdog.Start(outerCancellationToken);
+            return heartbeatWatchdog;
         }
 
-        public void Start(CancellationToken outerCancellationToken = default(CancellationToken))
+        public void Start(CancellationToken cancelToken = default(CancellationToken), DateTime? now = null)
         {
-            _innerCancellationTokenSource = new CancellationTokenSource();
-
-            _cancelSource = CancellationTokenSource.CreateLinkedTokenSource(
-                _innerCancellationTokenSource.Token,
-                outerCancellationToken);
-
             _infoLogger($"HeartbeatWatchdog {_name} has been started");
 
             Beat();
-            Task.Run(async () => await RunMonitor(_cancelSource.Token), _cancelSource.Token);
+            Task.Run(async () => await RunMonitor(cancelToken), cancelToken);
         }
 
-//        public void Restart(CancellationToken outerCancellationToken = default(CancellationToken))
-//        {
-//            _infoLogger($"HeartbeatWatchdog {_name} about to restart");
-//            _cancelSource.Cancel();
-//
-//            _cancelSource.Dispose();
-//            _innerCancellationTokenSource.Dispose();
-//
-//            Start(outerCancellationToken);
-//        }
-
-        public void Beat()
+        public void Beat(DateTime? now = null)
         {
-            Volatile.Write(ref _lastBeatTotalMs, DateTimeToTotalMs(DateTime.Now));
+            Volatile.Write(ref _lastBeatTotalMs, DateTimeToTotalMs(now ?? DateTime.Now));
             _debugLogger($"HeartbeatWatchdog {_name} got a beat");
-        }
-
-        public void Dispose()
-        {
-            _cancelSource.Dispose();
-            _innerCancellationTokenSource.Dispose();
         }
 
         private async Task RunMonitor(CancellationToken cancelToken)
